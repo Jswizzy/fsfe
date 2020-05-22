5d2f98c1ad755a93d8f91ae0add35100e105d9fc
Justin Smith <justinsmith@pop-os.localdomain>
2020-05-22 14:48:13 -0400

    feat(basic): added new path /demo

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/demo', (req, res) => {
+  res.set('X-full-stack', '4life')
+  res.status(418)
+  res.send('Demo')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
